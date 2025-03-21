5721b831e334794c51a8c061cc64f5265c187a3c
Matheus <theo.guimaraes@hotmail.com>
2025-03-21 17:13:04 -0300

    feat: add scoreboard functionality for win/loss tracking in LocalStorage; remove unused dependency

--- hooks/useLetterVerification.ts
+++ hooks/useLetterVerification.ts
@@ -1,7 +1,6 @@
 import GameContext from "@/contexts/GameContext";
-import { choicedWord } from "./choicedWord";
 import { useContext, useEffect } from "react";
-import { useGameStats } from "./useGameState";
+import { useGameStats } from "./useGameStats";
 
 export function useLetterVerification() {
   const context = useContext(GameContext);
@@ -10,14 +9,14 @@ export function useLetterVerification() {
     throw new Error("Not found a GameContext in useLetterVerification.ts");
   }
 
-  const { setCorrectLetters, setWrongLetters, correctLetters, wrongLetters } = context;
-  const { isUserWin, isUserLoss } = useGameStats();
-  const correctWord = choicedWord().word.split("");
+  const { setCorrectLetters, setWrongLetters, correctLetters, wrongLetters, word } =
+    context;
+  const { checkGameState } = useGameStats();
+  const correctWord = word.word.split("");
 
   useEffect(() => {
-    isUserWin();
-    isUserLoss();
-  }, [correctLetters, wrongLetters, isUserWin, isUserLoss])
+    checkGameState();
+  }, [correctLetters, wrongLetters, checkGameState]);
 
   function verifyWord(letter: string) {
     if (correctWord.includes(letter)) {
--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -1,22 +1,54 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LetterSelector from "../organisms/LetterSelector";
 import HangmanSide from "../organisms/HangmanSide";
 import GameContext from "@/contexts/GameContext";
+import LossGame from "../organisms/LossGame";
+import WinGame from "../organisms/WinGame";
+import { choicedWord } from "../../../hooks/choicedWord";
+import { GameState } from "@/enums/GameState";
 
 export default function HomeLayout() {
-
   const [correctLetters, setCorrectLetters] = useState<string[]>([]);
   const [wrongLetters, setWrongLetters] = useState<string[]>([]);
-  const [gameState, setGameState] = useState('')
+  const [gameState, setGameState] = useState(GameState.Initial);
+  const [chosenWord, setChosenWord] = useState<{ id: number; word: string; category: string }>({
+    id: 0,
+    word: "",
+    category: "",
+  });
+
+  useEffect(() => {
+    if (gameState === GameState.Initial) {
+      const SECRET_WORD = choicedWord();
+      const id = SECRET_WORD.id;
+      const word = SECRET_WORD.word.toUpperCase();
+      const category = SECRET_WORD.category;
+
+      setChosenWord({ id, word, category });
+      setGameState(GameState.Playing);
+    }
+  }, [gameState]);
 
   return (
     <div className="w-full">
       <div className="flex justify-between items-center h-full max-w-[900px] mx-auto">
-        <GameContext.Provider value={{ correctLetters, setCorrectLetters,  wrongLetters, setWrongLetters, gameState, setGameState }}>
+        <GameContext.Provider
+          value={{
+            word: chosenWord,
+            correctLetters,
+            setCorrectLetters,
+            wrongLetters,
+            setWrongLetters,
+            gameState,
+            setGameState,
+          }}
+        >
           <HangmanSide />
           <LetterSelector />
+          <WinGame />
+          <LossGame />
         </GameContext.Provider>
       </div>
     </div>
