d2402fe7ac65f6d97d452b8b16f23401f53a6364
Matheus <theo.guimaraes@hotmail.com>
2025-03-22 23:42:58 -0300

    feat: add Footer component and enhance Header styling for improved UI

---
+++ src/components/Footer.tsx
@@ -0,0 +1,90 @@
+import { Mail } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-7 mt-5 w-full">
+      <div className="flex justify-between max-w-[900px] mx-auto">
+        <div className="max-w-[450px] h-full">
+          <p className="text-xl font-semibold h-11 flex items-center">Objetivo</p>
+          <p className="mt-5">
+            Site criado por <span className="font-bold">Matheus Santos</span>{" "}
+            para testar e aprender novas funcionalidades do React, nesse projeto
+            em específico o foco compreender melhor o uso de:
+            <p className="italic">componentes, hooks e useContext.</p>
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-semibold h-11 flex items-center">Contato</p>
+          <Link
+            href={
+              "https://api.whatsapp.com/send?phone=5541984860525&text=Olá, vi sua página de filmes e gostaria de saber mais sobre seus serviços e verificar possíveis parcerias. Podemos conversar?"
+            }
+            target="_blank"
+            className="flex gap-4 mt-5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="24px"
+              y="24px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentcolor"
+            >
+              <path d="M 12.011719 2 C 6.5057187 2 2.0234844 6.478375 2.0214844 11.984375 C 2.0204844 13.744375 2.4814687 15.462563 3.3554688 16.976562 L 2 22 L 7.2324219 20.763672 C 8.6914219 21.559672 10.333859 21.977516 12.005859 21.978516 L 12.009766 21.978516 C 17.514766 21.978516 21.995047 17.499141 21.998047 11.994141 C 22.000047 9.3251406 20.962172 6.8157344 19.076172 4.9277344 C 17.190172 3.0407344 14.683719 2.001 12.011719 2 z M 12.009766 4 C 14.145766 4.001 16.153109 4.8337969 17.662109 6.3417969 C 19.171109 7.8517969 20.000047 9.8581875 19.998047 11.992188 C 19.996047 16.396187 16.413812 19.978516 12.007812 19.978516 C 10.674812 19.977516 9.3544062 19.642812 8.1914062 19.007812 L 7.5175781 18.640625 L 6.7734375 18.816406 L 4.8046875 19.28125 L 5.2851562 17.496094 L 5.5019531 16.695312 L 5.0878906 15.976562 C 4.3898906 14.768562 4.0204844 13.387375 4.0214844 11.984375 C 4.0234844 7.582375 7.6067656 4 12.009766 4 z M 8.4765625 7.375 C 8.3095625 7.375 8.0395469 7.4375 7.8105469 7.6875 C 7.5815469 7.9365 6.9355469 8.5395781 6.9355469 9.7675781 C 6.9355469 10.995578 7.8300781 12.182609 7.9550781 12.349609 C 8.0790781 12.515609 9.68175 15.115234 12.21875 16.115234 C 14.32675 16.946234 14.754891 16.782234 15.212891 16.740234 C 15.670891 16.699234 16.690438 16.137687 16.898438 15.554688 C 17.106437 14.971687 17.106922 14.470187 17.044922 14.367188 C 16.982922 14.263188 16.816406 14.201172 16.566406 14.076172 C 16.317406 13.951172 15.090328 13.348625 14.861328 13.265625 C 14.632328 13.182625 14.464828 13.140625 14.298828 13.390625 C 14.132828 13.640625 13.655766 14.201187 13.509766 14.367188 C 13.363766 14.534188 13.21875 14.556641 12.96875 14.431641 C 12.71875 14.305641 11.914938 14.041406 10.960938 13.191406 C 10.218937 12.530406 9.7182656 11.714844 9.5722656 11.464844 C 9.4272656 11.215844 9.5585938 11.079078 9.6835938 10.955078 C 9.7955938 10.843078 9.9316406 10.663578 10.056641 10.517578 C 10.180641 10.371578 10.223641 10.267562 10.306641 10.101562 C 10.389641 9.9355625 10.347156 9.7890625 10.285156 9.6640625 C 10.223156 9.5390625 9.737625 8.3065 9.515625 7.8125 C 9.328625 7.3975 9.131125 7.3878594 8.953125 7.3808594 C 8.808125 7.3748594 8.6425625 7.375 8.4765625 7.375 z"></path>
+            </svg>
+            <span className="hover:underline">+55 41 98486-0525</span>
+          </Link>
+          <Link
+            href={"mailto:theo.guimaraes@hotmail.com"}
+            className="flex gap-4 mt-3"
+          >
+            <Mail />
+            <span className="hover:underline">theo.guimaraes@hotmail.com</span>
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/math-guima/"}
+            target="_blank"
+            className="flex gap-4 mt-3"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+              <rect width="4" height="12" x="2" y="9" />
+              <circle cx="4" cy="4" r="2" />
+            </svg>
+            <span className="hover:underline">linkedin.com/in/math-guima/</span>
+          </Link>
+          <Link
+            href={"https://github.com/HolaSoyMath"}
+            target="_blank"
+            className="flex gap-4 mt-3 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+              <path d="M9 18c-4.51 2-5-2-7-2" />
+            </svg>
+            <span className="hover:underline">github.com/HolaSoyMath</span>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return(
     <header className='w-full h-16'>
       <div className='flex justify-between items-center h-full max-w-[900px] mx-auto px-10 lg:px-0'>
-        <p>Jogo da Forca</p>
+        <p className='text-2xl font-semibold'>Jogo da Forca</p>
         <div className='flex gap-4 items-center'>
           <ToggleTheme />
           {/* <p>Entrar</p> */}
