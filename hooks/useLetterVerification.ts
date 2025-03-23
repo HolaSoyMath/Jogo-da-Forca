fb9c3b745bd5eceddcea1a50a2361e528c363f1a
Matheus <theo.guimaraes@hotmail.com>
2025-03-22 22:25:59 -0300

    feat: update game state management; add modal handling for win/loss and improve reset functionality

--- hooks/useLetterVerification.ts
+++ hooks/useLetterVerification.ts
@@ -18,8 +18,6 @@ export function useLetterVerification() {
   useEffect(() => {
     checkGameState();
   }, [correctLetters, wrongLetters, checkGameState]);
-    checkGameState();
-  }, [correctLetters, wrongLetters, checkGameState]);
 
   function verifyWord(letter: string) {
     if (correctWord.includes(letter)) {
